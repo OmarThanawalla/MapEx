6763bc52e243860f5c19c1d76b99f7f51be16108
Omar Thanawalla <omar.thanawalla@gmail.com>
2013-08-07 19:32:09 -0500

    initial commit

---
+++ MapExperience/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapExperience
+//
+//  Created by Omar Thanawalla on 8/7/13.
+//  Copyright (c) 2013 Omar Thanawalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapExperience/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapExperience
+//
+//  Created by Omar Thanawalla on 8/7/13.
+//  Copyright (c) 2013 Omar Thanawalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
