cd0deb17f1a8f577f04201f94fe5ff41f24d9d48
Omar Thanawalla <omar.thanawalla@gmail.com>
2013-08-13 21:44:41 -0500

    got afnetworking to pull something

--- MapExperience/ViewController.m
+++ MapExperience/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <AFNetworking.h>
 @interface ViewController ()
 
 @end
@@ -20,6 +20,29 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     // Talk to Yelp Api and get the data points for the businesses
+    AFHTTPClient *httpClient = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://api.openweathermap.org/"]];
+    httpClient.parameterEncoding = AFJSONParameterEncoding;
+    [httpClient setDefaultHeader:@"Accept" value:@"application/json"];
+    [httpClient registerHTTPOperationClass:[AFJSONRequestOperation class]];
+    
+    //[myClient setDefaultHeader:@"Accept" value:@"application/json"];
+    
+    [httpClient getPath:@"data/2.5/weather?q=London,uk" parameters:nil
+        success:^(AFHTTPRequestOperation *operation, id responseObject)
+    {
+        //Success code
+        NSLog(@"We have success!!");
+        NSLog(@"This is what responseObject look like: %@",responseObject);
+        
+        
+    }
+        failure:^(AFHTTPRequestOperation *operation, NSError *error)
+    {
+        //Error code
+        NSLog(@"We did NOT have success");
+    }
+     ];
+    
     
 }
 
